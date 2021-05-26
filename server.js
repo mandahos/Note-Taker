3f16747b101d2dae8730424d4f916463a88391f6
Amanda Hoshor <hoshor.manda@gmail.com>
2021-05-25 22:21:11 -0600

    set up server port

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3016;
+
+app.use()
+
+app.listen(PORT, () => {
+    console.log(`API server now on port 3016!`);
+});
