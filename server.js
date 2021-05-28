4fb9dbe9dcd85d5f649b6590f7a1027346925bcb
Amanda Hoshor <hoshor.manda@gmail.com>
2021-05-27 20:20:02 -0600

    adding heroku?

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3016;
 
-app.use()
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`API server now on port 3016!`);
