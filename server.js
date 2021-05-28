b294f30c1cb7c30113c9dd9545266556feaf3819
Amanda Hoshor <hoshor.manda@gmail.com>
2021-05-27 20:56:25 -0600

    adding get routes

--- server.js
+++ server.js
@@ -2,11 +2,38 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
+
 
 const PORT = process.env.PORT || 3016;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
+
+const writefileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+let allNotes;
+
+//Rutes, setting up the server by routes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+
+app.get("api/notes", (req, res) => {
+    readFileAsync(path.join(__dirname, ".db.db.json"), "utf8")
+    .then(function (data) {
+        return res.json(JSON.parse(data));
+    });
+});
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port 3016!`);
